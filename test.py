35ca255c401171684e5d1a8252dedc4e12f0fc24
godfreykaris <godfreykarisw@gmail.com>
2023-10-14 23:27:59 +0300

    User registration

---
+++ test.py
@@ -0,0 +1,27 @@
+import requests
+
+# Define the URL for the registration endpoint
+url = 'http://localhost:5000/api/register'
+
+# Define the data you want to send as a dictionary
+data = {
+    'username': 'me',
+    'email': 'you@gmail.com',
+    'password': 'iampassword12',
+    'confirmPassword': 'iampassword12'  # Confirm password can be the same as the password
+}
+
+# Send a POST request with the data
+response = requests.post(url, json=data)  # Using json to automatically serialize the data as JSON
+
+if response.status_code == 200:
+    data = response.json()
+    message = data['message']
+    print(message)
+else:
+    error_data = response.json()
+    error_message = error_data['message']
+    error_code = response.status_code
+    print(f'Error {error_code}: {error_message}')
+
+    
